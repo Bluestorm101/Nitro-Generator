9be91e0b68bd4591080ff5b4a29949a162539960
Bluestorm101 <114834065+Bluestorm101@users.noreply.github.com>
2022-10-01 21:50:48 +0100

    Add files via upload

---
+++ PromoGen.py
@@ -0,0 +1,44 @@
+import time
+import os 
+import sys
+import random
+import datetime
+import art 
+from art import *
+import colorama
+from colorama import Fore
+
+
+
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghijklmnopqrstuvwxyz0123456789"
+discord = "https://discord.gift/"
+
+
+gen = 0
+
+
+print("Scraping 28731 Proxies")
+
+time.sleep(0.5)
+
+print("Connecting to Proxies")
+
+time.sleep(5)
+
+os.system('CLS')
+tprint("bluestorm")
+print("This Just For Fun it would probaly take like 12 billion codes are more to get an real one ")
+promo = int(input("Amount of Promos code to generate:\n"))
+
+
+for i in range(promo):
+    code = ''.join((random.choice(chars) for i in range(16)))
+    
+    result =  '[+]' + discord + code
+    
+    
+    
+    print(Fore.CYAN + result)
+    output = open("nitrocodes.txt", "a")
+    output.write( result + "\n")
+
